01e918f30a9c95d9893f2b661e5dbfd97f110e5e
sciencemj <sciencemj@naver.com>
2022-08-05 16:55:41 +0900

    commit
    wallet manager and command added

--- src/main/java/sciencemj/shop/Shop.java
+++ src/main/java/sciencemj/shop/Shop.java
@@ -1,26 +1,54 @@
 package sciencemj.shop;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public final class Shop extends JavaPlugin {
-    public List<Player> wallet = new ArrayList<Player>();
+    public static Inventory inv; // 인벤토리 기본 모델 -> 플레이어마다 인스턴트 생성 -> 리스트로 관리
     @Override
     public void onEnable() {
         // Plugin startup logic
         PluginDescriptionFile file = this.getDescription();
-
-        System.out.println(file.getName() + "version:" + file.getVersion() + " loaded");
-
-        this.getServer().getPluginManager().registerEvents(new ShopEventHandler(), this);
+        System.out.println(file.getName() + "version:" + file.getVersion() + " loaded"); //print description
+        this.getServer().getPluginManager().registerEvents(new ShopEventHandler(), this); //add event listener
+        this.getCommand("money").setExecutor((CommandExecutor) new ShopCommand());
 
         for(Player p : this.getServer().getOnlinePlayers()){
             p.sendMessage("hi");
         }
+        inv = Bukkit.createInventory(null, 7, "WALLET");
+        itemFill(inv, createItem(Material.BLUE_STAINED_GLASS, ""), 1,7); //fill inv 1 to 7
+        inv.setItem(3, createItem(Material.GOLD_NUGGET, "잔액"));
+
+    }
+
+    public ItemStack createItem(Material material, @NotNull String name) {
+        ItemStack itemStack = new ItemStack(material, 1);
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.setDisplayName(name);
+        itemStack.setItemMeta(meta);
+        return itemStack;
+    }
+
+    public void itemFill(Inventory inventory, ItemStack item, int countStart, int countEnd) { //[1,2,3,4,5,6,7...]
+        if (countEnd == countStart - 1) {
+            return;
+        }
+        inventory.setItem(countEnd - 1, item);
+        itemFill(inventory, item, countStart, countEnd - 1);
     }
 
     @Override
---
+++ src/main/java/sciencemj/shop/ShopCommand.java
@@ -0,0 +1,39 @@
+package sciencemj.shop;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class ShopCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
+        if (cmd.getName().equals("money")){
+            if (sender instanceof Player){
+                if(args.length < 2){
+                    sender.sendMessage("this command require at least 2 args");
+                }else {
+                    Player p = Bukkit.getPlayer(args[0]);
+                    if(args.length == 2){
+                        sender.sendMessage(p.getName() + "'s money: " + wM.get(p));
+                    }else if (args.length == 3){
+                        double amount = Double.parseDouble(args[2]);
+                        switch (args[1]) {
+                            case "set" -> {
+                                wM.set(p, amount);
+                                sender.sendMessage(p.getName() + "'s money is now: " + wM.get(p));
+                            }
+                            case "add" -> {
+                                wM.add(p, amount);
+                                sender.sendMessage(p.getName() + "'s money is now: " + wM.get(p));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
